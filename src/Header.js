0808754989b72812b342925b033fe65349b5ad56
Rinaldo23 <[rinaldobadigar98@gmail.com]>
2022-12-14 19:45:34 +0530

    NavBar-UI  Implemented

---
+++ src/Header.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import SearchIcon from '@mui/icons-material/Search';
+import HomeIcon from '@mui/icons-material/Home';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import MessageRoundedIcon from '@mui/icons-material/MessageRounded';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import './Header.css'
+import HeaderOption from './HeaderOption';
+
+function Header() {
+    return (
+        <div className='header'>
+
+            <div className='header__left'>
+                <LinkedInIcon className='linkedin-logo'/>
+                <div className='header__search'>
+                    <SearchIcon />
+                    <input type='text'></input>
+                </div>
+            </div>
+
+            <div className='header__right'>
+                <HeaderOption Icon={HomeIcon} title="Home"/>
+                <HeaderOption Icon={PeopleAltIcon} title="My Network"/>
+                <HeaderOption Icon={BusinessCenterIcon} title="Jobs"/>
+                <HeaderOption Icon={MessageRoundedIcon} title="Messaging"/>
+                <HeaderOption Icon={NotificationsIcon} title="Notifications"/>
+                <HeaderOption avatar={"https://upload.wikimedia.org/wikipedia/commons/8/8c/Cristiano_Ronaldo_2018.jpg"} title="Ronaldo"/>
+            </div>
+
+        </div>
+    )
+}
+
+export default Header
---
+++ src/HeaderOption.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './HeaderOption.css'
+import Avatar from '@mui/material/Avatar';
+
+function HeaderOption({avatar, Icon, title}) {
+  return (
+    <div className='headerOption'>
+        {Icon && <Icon className="HeaderOption__icon"/>}
+        {avatar && <Avatar className="HeaderOption__icon" alt="Remy Sharp" src={avatar} />}
+        <p>{title}</p>
+    </div>
+  )
+}
+
+export default HeaderOption
